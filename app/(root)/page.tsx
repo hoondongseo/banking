22742c12ff367927896289e6076f918efd3200a5
Donghoon Seo <naosdh@naver.com>
2025-01-25 19:19:36 +0900

    make right sidebar

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,9 @@
 import HeaderBox from '@/components/HeaderBox'
+import RightSidebar from '@/components/RightSidebar';
 import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
-    const loggedIn = { firstName: 'Donghoon' };
+    const loggedIn = { firstName: 'Donghoon', lastName: 'Seo', email: 'naosdh@naver.com' };
 
     return (
         <section className='home'>
@@ -21,7 +22,15 @@ const Home = () => {
                         totalCurrentBalance={1250.35}
                     />
                 </header>
-            </div>
+
+                RECENT TRANSATIONS
+                </div>
+
+                <RightSidebar
+                    user={loggedIn}
+                    transactions={[]}
+                banks={[{ currentBalance: 123.50 }, { currentBalance: 500.50 }]}
+                />
         </section>
     )
 }
